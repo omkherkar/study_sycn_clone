9b9cb14da7d00494339d9dd7ac45f5a93c6850c6
om kherkar <kherkarvivek@gmail.com>
2025-08-18 20:31:23 +0530

    transfer

--- frontend/ind.js
+++ frontend/ind.js
@@ -1,3 +1,29 @@
+let x = document.getElementById("mdrp");
+let m = document.getElementById("menudrop");
+let cr = document.getElementById("cross");
+let dr = document.getElementById("dropdown");
+let scr = document.getElementById("scroll")
+try{
+
+x.addEventListener('click',function(){
+
+    
+    dr.classList.add("d-none");
+    scr.classList.remove("d-none");
+});
+
+cr.addEventListener('click', async function(){
+     console.log("hello");
+     
+     await scr.classList.add("ani");
+     scr.classList.add("d-none");
+     dr.classList.remove("d-none");
+    
+ });
+
+scr.addEventListener("mouseup",function(){
+    m.scroll(0,10);
+});
 
 
 
@@ -22,3 +48,8 @@ function drop2(){
 let a = document.getElementById("idp");
  a.addEventListener('mouseover', drop);
  a.addEventListener("mouseout", drop2);
+
+}
+ catch(err){
+    console.log("hello" +err + err.stack);
+ }
